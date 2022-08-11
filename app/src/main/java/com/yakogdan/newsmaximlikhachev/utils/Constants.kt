1064a1962913c5d94e015af2d074bd6288f60e20
yakogdan <yakogdan@gmail.com>
2022-08-11 12:37:29 +0800

    Lesson_3

---
+++ app/src/main/java/com/yakogdan/newsmaximlikhachev/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.yakogdan.newsmaximlikhachev.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "fdd1693e145d40c78cc35c940dbbb42a"
+    }
+}
